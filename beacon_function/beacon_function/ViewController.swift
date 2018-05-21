7b4e43cfa188a722bca75468971b8e5873707198
Snow.yamada <s.yamada@snowrobin.jp>
2018-05-21 22:37:02 +0900

    データ取得成功

--- beacon_function/beacon_function/ViewController.swift
+++ beacon_function/beacon_function/ViewController.swift
@@ -11,28 +11,18 @@ import UIKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
-    
+
     // 受信行う
     var locationManager: CLLocationManager!
     // 発信を行う
     var beaconRegion: CLBeaconRegion!
-    let uuidString: String = "242A4B01-BD42-2CE0-9300-BA790F8FACBA"
+    let uuidString: String = "48534442-4C45-4144-80C0-1800FFFFFFFF"
     let beaconIdentifier = "BBBDFU"
-    
+
     // Mark: - IBOutlet
     @IBOutlet weak var label1: UILabel!
-    @IBOutlet weak var label2: UILabel!
-    
-    // Mark: - IBAction
-    @IBAction func button1(_ sender: UIButton) {
-        if (label1.text == "Immediate") {
-            label2.text = "ポイントゲットしましたー！"
-        }
-    }
-    @IBAction func button2(_ sender: UIButton) {
-        label2.text = ""
-    }
-    
+    @IBOutlet weak var label2:UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager = CLLocationManager()
@@ -43,18 +33,19 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // 位置情報取得感覚を設定
         // 1メートル離れたら位置情報を更新する
         locationManager.distanceFilter = 1
-        
+
         // 位置情報の利用許可を確認
         let status = CLLocationManager.authorizationStatus()
         print("CLAuthorizedStatus: \(status.rawValue)")
         // 位置情報の利用ステータスがまだ設定されていない場合
         if (status == .notDetermined) {
-            // バックグラウンドも常に許可に変更する
+            // 「常に許可」への変更をされるダイアログを表示する
+            // 容認するとバックグラウンドでも位置情報が利用される
             locationManager.requestAlwaysAuthorization()
         }
-        
+
     }
-    
+
     private func startMyMonitoring() {
         // ビーコン領域(発信を行うオブジェクト)生成
         guard let uuid = UUID(uuidString: uuidString) else {return}
@@ -99,8 +90,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         // 領域の内部にいるかの判定
         manager.requestState(for: region);
     }
-    
-    
+
+
     // requestStateが呼ばれると非同期で呼ばれる
     // 現在リージョン内にいるかどうかの通知(結果)を受け取る
     // 結果はstateの中に入っている
@@ -119,7 +110,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
             break
         }
     }
-    
+
     // startRangingBeaconsが呼ばれると呼ばれる
     // リージョン内にあるビーコンが検知され、引数beaconsに格納して渡されている
     // regionが開始されると1秒ごとに呼ばれるため、beaconsの中にbeaconある場合のみ処理を実行する
@@ -127,26 +118,48 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         if beacons.count > 0 {
             print("beaconsの中身: \(beacons)")
             let beacon = beacons[0]
-            print(beacon.proximityUUID)
-            print(beacon.major)
-            print(beacon.minor)
-            print(beacon.rssi)
             
+            let beaconUUID = beacon.proximityUUID;
+            let minorID = beacon.minor;
+            let majorID = beacon.major;
+            let rssi = beacon.rssi;
+            var proximity = ""
+            switch(beacon.proximity) {
+            case .immediate:
+                proximity = "かなり近い"
+                break
+            case .near:
+                proximity = "近い"
+                break
+            case .far:
+                proximity = "遠い"
+                break
+            case .unknown:
+                proximity = "測定不可"
+                break
+            }
+            var myBeaconDetails = "Major: \(majorID) "
+            myBeaconDetails += "Minor: \(minorID) "
+            myBeaconDetails += "Proximity:\(proximity) "
+            myBeaconDetails += "RSSI:\(rssi)"
+            print("myBeaconDetailsの値:\(myBeaconDetails)")
+            label1.text = "距離 \(proximity)"
+            label2.text = "RSSI \(rssi)"
         }
-        
+
     }
-    
+
     // Mark: IN/OUT検知 - delegate
     // IN - 領域内に入った際に通知
     func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
-        
+
         print("リージョンに入った")
         // CLBeaconRegionオブジェクトの持つ情報の取得
         manager.startRangingBeacons(in: region as! CLBeaconRegion)
         // ユーザーの位置情報を更新して確認
         manager.startUpdatingLocation()
     }
-    
+
     func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
         print("リージョンから出た")
         manager.stopRangingBeacons(in: region as! CLBeaconRegion)
